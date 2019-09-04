e211fed3d7fb27ed5afbab63c898ad174cd15ad2
deraldoduran <42320816+deraldoduran@users.noreply.github.com>
2019-09-04 15:54:29 -0300

    Update CaSa.java
    já está com o array list

--- execap4program2/src/execap4program2/CaSa.java
+++ execap4program2/src/execap4program2/CaSa.java
@@ -4,11 +4,39 @@
  * and open the template in the editor.
  */
 package execap4program2;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  *
  * @author eu
  */
 public class CaSa {
+    String cor;
+    ArrayList<Porta> portas= new ArrayList<Porta>();
     
+    
+
+    public CaSa(String cor) {
+        this.cor = cor;
+    }
+    
+    public void contaportaaberta() {
+        int cont=0;
+        for(Porta i: portas){
+            if(i.isAberta()){
+                cont ++;
+            }
+        }
+         System.out.println("A quantidade de portas abertas na casa"+" "+cont); 
+    }
+    
+    public void adicionarPorta(){
+        System.out.println("digite uma cor");
+        String corporta;
+        Scanner s=new Scanner(System.in); 
+        corporta=s.next();
+        Porta b=new Porta(corporta);
+        this.portas.add(b);
+        
+    }
 }
